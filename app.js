67e84c42fef5935aa449e477dbfaaf5311a32f52
sunpengfei <rrlyrstar@163.com>
2020-06-13 16:26:23 +0800

    跨域

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ require('./activity');
 // log request URL:
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
-    await next();
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    if (ctx.method == 'OPTIONS') {
+        ctx.body = 200;
+    } else {
+        await next();
+    }
 });
 
 // parse request body:
