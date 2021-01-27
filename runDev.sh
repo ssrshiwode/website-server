05b60a206c4b810ffeddae688f0f77912335e415
zhaolaishuang <940108360@qq.com>
2021-01-27 11:21:07 +0800

    up

--- runDev.sh
+++ runDev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pm2 stop website-server
-pm2 delete website-server
-pm2 start /data/node/website-server/app.js --name=website-server --log-date-format="YYYY-MM-DD HH:mm Z" -o /data/logs/pm2/website-server.log -e /data/logs/pm2/website-server.err
+/usr/local/share/node-v12.14.1-linux-x64/bin/pm2 stop website-server
+/usr/local/share/node-v12.14.1-linux-x64/bin/pm2 delete website-server
+/usr/local/share/node-v12.14.1-linux-x64/bin/pm2 start /data/node/website-server/app.js --name=website-server --log-date-format="YYYY-MM-DD HH:mm Z" -o /data/logs/pm2/website-server.log -e /data/logs/pm2/website-server.err
